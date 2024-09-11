a5eaf9a7adaa3ef8482c4c638d3b80e85f201acf
harrison <harrison@siliconii.net>
2024-09-11 11:55:32 +0900

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Header from "./components/organisms/Header";
 import './App.css';
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Header/>
             <AppRoutes/>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
