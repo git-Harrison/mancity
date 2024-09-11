ece86a900e229899adcdcf88f63a1739a161b4f7
harrison <harrison@siliconii.net>
2024-09-11 16:00:11 +0900

    정령

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import MainPage from "../views/MainPage";
 import PlayerPage from "../views/PlayerPage";
 
 const AppRoutes: React.FC = () => {
     return (
         <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/players" element={<PlayerPage />} />
+            <Route path="/" element={<MainPage/>}/>
+            <Route path="/players" element={<PlayerPage/>}/>
         </Routes>
     );
 };
