bea6025d0b55cb9d7c20e9424adae126ff72d734
harrison <harrison@siliconii.net>
2024-10-08 09:26:31 +0900

    테스트

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// src/config.ts
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-export const API_BASE_URL = isDevelopment
-    ? '/api/football' // 로컬 개발 환경: 프록시 사용
-    : 'https://api.football-data.org/v4'; // 배포 환경: 실제 API 서버 사용
