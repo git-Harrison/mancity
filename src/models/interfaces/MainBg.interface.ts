dd4938a0d8ddd1f0d174f4eca11b2f2070d8baf7
harrison <harrison@siliconii.net>
2024-10-08 17:38:04 +0900

    메인페이지 삭제 & 이미지 삭제

--- src/models/interfaces/MainBg.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface MainBgProps {
-    bgImgUrl: string;
-    isFirstLoad: boolean;
-}
--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -1,28 +1,9 @@
-import React, {useState} from "react";
-import MainSlider from "../components/organisms/MainSlider";
-import MainBg from "../components/organisms/MainBg";
-import {useMainPageViewModel} from "../viewmodels/useMainPageViewModel";
+import React from "react";
 
 const MainPage: React.FC = () => {
-    const {slides} = useMainPageViewModel();
-
-    const [selectedSlideId, setSelectedSlideId] = useState<number | null>(null);
-    const selectedSlide = slides.find(slide => slide.id === selectedSlideId);
-    const bgImg = selectedSlide ? selectedSlide.bgImg : slides[0]?.bgImg;
-    const [isFirstLoad, setIsFirstLoad] = useState(true);
-
-    const handleSlideChange = (newSlideId: number) => {
-        setSelectedSlideId(newSlideId);
-        if (isFirstLoad) setIsFirstLoad(false);
-    };
-
     return (
         <div className="container">
-            {/*<MainBg bgImgUrl={bgImg} isFirstLoad={isFirstLoad}/>*/}
-            {/*<MainSlider*/}
-            {/*    slides={slides}*/}
-            {/*    setSelectedSlideId={handleSlideChange}*/}
-            {/*/>*/}
+            test
         </div>
     );
 };
