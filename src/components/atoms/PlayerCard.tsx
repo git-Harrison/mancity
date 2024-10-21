312cb0ef3f27fd493c4a4c1051e071d6673499ff
Harrison <wognsl305@naver.com>
2024-10-21 16:20:27 +0900

    플레이어 카드 이미지교체 & UI 수정

--- src/components/atoms/PlayerCard.tsx
+++ src/components/atoms/PlayerCard.tsx
@@ -10,50 +10,51 @@ import {getCardImages} from '../../utils/getCardImages';
  */
 
 const PlayerCard: React.FC<PlayerCardProps & { showEnhancement?: boolean }> = ({player, showEnhancement = false}) => {
-    const isIconNumbers = [1042, 101008, 101049, 101037, 1051].includes(player.number);
-    const {background: backgroundImage, tag: tagImage, badge: badgeImage} = getCardImages(player);
+    const cardType = [1042, 101008, 101049, 101037, 1051, 101032, 101010, 101021].includes(player.number) ? 'bdo' :
+        [9, 17, 16, 20, 47, 19, 3, 24, 1026, 101007, 101019, 1055, 101055, 101056].includes(player.number) ? 'dld' : '';
+    const {background: backgroundImage, league: leagueIcon, tag: tagImage} = getCardImages(player);
 
     return (
-        <div
-            className="players-card"
-            style={{
-                backgroundImage: `url(${backgroundImage})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                width: '210px',
-                height: '336px',
-                textAlign: 'center',
-            }}
-        >
-            <div className="players-row">
-                <div className={`number ${isIconNumbers ? 'icon' : ''}`}>{player.overall_ability}</div>
-                <div className={`position ${player.position} ${isIconNumbers ? 'icon' : ''}`}>{player.position}</div>
-                {showEnhancement && (
-                    <div className={`enhancement-level enhancement-level${player.enhancementLevel}`}>
-                        {player.enhancementLevel}
+
+        <div className="players-card-row" style={{backgroundImage: `url(${backgroundImage})`,}}>
+            <div className="players-card">
+                <div className="players-row">
+                    <div className={`number ${cardType}`}>{player.overall_ability}</div>
+                    <div
+                        className={`position ${player.position} ${cardType}`}>{player.position}</div>
+                    {showEnhancement && (
+                        <div className={`enhancement-level enhancement-level${player.enhancementLevel}`}>
+                            {player.enhancementLevel}
+                        </div>
+                    )}
+                    <div className="img">
+                        <img
+                            src={`${process.env.PUBLIC_URL}/images/profile_icon_${player.number}.webp`}
+                            alt="players-face"
+                            onError={(e) => {
+                                const target = e.target as HTMLImageElement;
+                                target.onerror = null;
+                                target.src = `${process.env.PUBLIC_URL}/images/profile_icon_default.webp`;
+                            }}
+                        />
                     </div>
-                )}
-                <div className="country-logo">
-                    <img src={getFlagImage(player.nationality)} alt={player.nationality}/>
-                </div>
-                <div className="img">
-                    <img
-                        src={`${process.env.PUBLIC_URL}/images/profile_icon_${player.number}.webp`}
-                        alt="players-face"
-                        onError={(e) => {
-                            const target = e.target as HTMLImageElement;
-                            target.onerror = null;
-                            target.src = `${process.env.PUBLIC_URL}/images/profile_icon_default.webp`;
-                        }}
-                    />
-                </div>
-                <div className="team-logo">
-                    <img src={badgeImage} alt="team-logo"/>
-                </div>
-                <div className="players">
-                    <img src={tagImage} alt="card_icon"/>
-                    <div className="name">{player.name}</div>
+                    <div className="players">
+                        <div className="players-detail-top">
+                            <img src={tagImage} alt="card_icon"/>
+                            <div className={`name ${cardType}`}>{player.name}</div>
+                        </div>
+                        <div className="players-detail-bottom">
+                            <div className="country-logo">
+                                <img src={getFlagImage(player.nationality)} alt={player.nationality}/>
+                            </div>
+                            <div className="league-logo">
+                                <img src={leagueIcon}/>
+                            </div>
+                            <div className="city-logo">
+                                <img src={`${process.env.PUBLIC_URL}/images/player_card_city_icon.webp`}/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/utils/getCardImages.ts
+++ src/utils/getCardImages.ts
@@ -1,74 +1,83 @@
 import {Player} from '../models/interfaces/Player.interface';
 
-const iconNumbers = [1042, 101008, 101049, 101037, 1051];
+const dboNumbers = [1042, 101008, 101049, 101037, 1051, 101032, 101010, 101021];
+const dldNumbers = [9, 17, 16, 20, 47, 19, 3, 24, 1026, 101007, 101019, 1055, 101055, 101056];
 
-// getCardImages 함수 정의
 export const getCardImages = (player: Player) => {
-    const isIconNumbers = iconNumbers.includes(player.number);
+    const isBboNumbers = dboNumbers.includes(player.number);
+    const isBldNumbers = dldNumbers.includes(player.number);
 
-    if (isIconNumbers) {
+    if (isBldNumbers) {
         return {
-            background: `${process.env.PUBLIC_URL}/images/card_icon_bg.png`,
-            tag: `${process.env.PUBLIC_URL}/images/card_icon_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_icon_badge.png`
+            background: `${process.env.PUBLIC_URL}/images/card_bld_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_white_icon.webp`,
+            tag: `${process.env.PUBLIC_URL}/images/card_bld_tag.png`,
+        };
+    }
+
+    if (isBboNumbers) {
+        return {
+            background: `${process.env.PUBLIC_URL}/images/card_bdo_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_bdo_icon.webp`,
+            tag: `${process.env.PUBLIC_URL}/images/card_bdo_tag.png`,
         };
     }
 
     if (player.transfer_date) {
         return {
             background: `${process.env.PUBLIC_URL}/images/card_toty_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_icon.webp`,
             tag: `${process.env.PUBLIC_URL}/images/card_toty_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_toty_badge.png`
         };
     }
 
     if (player.overall_ability < 60) {
         return {
             background: `${process.env.PUBLIC_URL}/images/card_default_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_icon.webp`,
             tag: `${process.env.PUBLIC_URL}/images/card_default_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_default_badge.png`
         };
     } else if (player.overall_ability < 70) {
         return {
             background: `${process.env.PUBLIC_URL}/images/card_tt_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_icon.webp`,
             tag: `${process.env.PUBLIC_URL}/images/card_tt_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_tt_badge.png`
         };
     } else if (player.overall_ability < 80) {
         return {
-            background: `${process.env.PUBLIC_URL}/images/card_fc_bg.png`,
-            tag: `${process.env.PUBLIC_URL}/images/card_fc_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_fc_badge.png`
+            background: `${process.env.PUBLIC_URL}/images/card_cc_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_icon.webp`,
+            tag: `${process.env.PUBLIC_URL}/images/card_cc_tag.png`,
         };
     } else if (player.overall_ability < 90) {
         return {
-            background: `${process.env.PUBLIC_URL}/images/card_cc_bg.png`,
-            tag: `${process.env.PUBLIC_URL}/images/card_cc_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_cc_badge.png`
+            background: `${process.env.PUBLIC_URL}/images/card_ld_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_icon.webp`,
+            tag: `${process.env.PUBLIC_URL}/images/card_ld_tag.png`,
         };
     } else if (player.overall_ability < 100) {
         return {
-            background: `${process.env.PUBLIC_URL}/images/card_ld_bg.png`,
-            tag: `${process.env.PUBLIC_URL}/images/card_ld_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_ld_badge.png`
+            background: `${process.env.PUBLIC_URL}/images/card_jnm_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_icon.webp`,
+            tag: `${process.env.PUBLIC_URL}/images/card_jnm_tag.png`,
         };
     } else if (player.overall_ability < 110) {
         return {
-            background: `${process.env.PUBLIC_URL}/images/card_jnm_bg.png`,
-            tag: `${process.env.PUBLIC_URL}/images/card_jnm_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_jnm_badge.png`
+            background: `${process.env.PUBLIC_URL}/images/card_ut_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_icon.webp`,
+            tag: `${process.env.PUBLIC_URL}/images/card_ut_tag.png`,
         };
     } else if (player.overall_ability < 120) {
         return {
-            background: `${process.env.PUBLIC_URL}/images/card_ut_bg.png`,
-            tag: `${process.env.PUBLIC_URL}/images/card_ut_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_ut_badge.png`
+            background: `${process.env.PUBLIC_URL}/images/card_cu_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_white_icon.webp`,
+            tag: `${process.env.PUBLIC_URL}/images/card_cu_tag.png`,
         };
     } else {
         return {
             background: `${process.env.PUBLIC_URL}/images/card_default_bg.png`,
+            league: `${process.env.PUBLIC_URL}/images/player_card_league_icon.webp`,
             tag: `${process.env.PUBLIC_URL}/images/card_default_tag.png`,
-            badge: `${process.env.PUBLIC_URL}/images/card_default_badge.png`
         };
     }
 };
