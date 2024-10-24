770dfb56e23eb1495c2b9ac3efb2b012e4bea1ce
Harrison <wognsl305@naver.com>
2024-10-24 17:14:07 +0900

    로딩 배경 이미지 최적화

--- src/components/organisms/LoadingScreen.tsx
+++ src/components/organisms/LoadingScreen.tsx
@@ -5,7 +5,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({className}) => {
     return (
         <div className={`loading-screen ${className ? className : ""}`}>
             <div className="loading-bg"
-                 style={{backgroundImage: `url(${process.env.PUBLIC_URL}/man-city-bg.jpg)`}}></div>
+                 style={{backgroundImage: `url(${process.env.PUBLIC_URL}/man-city-bg.webp)`}}></div>
             <img src={`${process.env.PUBLIC_URL}/man-city-logo.webp`} alt="Manchester City Logo"
                  className="loading-logo"/>
         </div>
