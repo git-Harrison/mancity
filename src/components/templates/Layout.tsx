78df9313e5cc3de541b14ca77a0e3b5e9f69407f
Harrison <wognsl305@naver.com>
2024-10-25 12:05:32 +0900

    백그라운드 이미지 초기 로딩 + 라이브러리 추가

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {HelmetProvider, Helmet} from 'react-helmet-async';
 import Header from '../organisms/Header';
 import SideMenuNav from '../organisms/SideMenuNav';
 import AppRoutes from '../../routes/AppRoutes';
@@ -8,12 +9,19 @@ const Layout: React.FC = () => {
     const {isDarkMode, toggleTheme} = useThemeViewModel();
 
     return (
-        <>
-            <img src={`${process.env.PUBLIC_URL}/images/bg.webp`} alt="" className="contents-background-image"/>
+        <HelmetProvider>
+            <Helmet>
+                <link rel="preload" as="image" href={`${process.env.PUBLIC_URL}/images/bg.webp`}/>
+            </Helmet>
+            <img
+                src={`${process.env.PUBLIC_URL}/images/bg.webp`}
+                alt="Background"
+                className="contents-background-image"
+            />
             <Header toggleTheme={toggleTheme} isDarkMode={isDarkMode}/>
             <SideMenuNav/>
             <AppRoutes/>
-        </>
+        </HelmetProvider>
     );
 };
 
