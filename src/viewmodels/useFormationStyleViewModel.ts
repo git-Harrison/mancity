1b243fd449b71266696427ba4f08d52ce3ed674c
Harrison <wognsl305@naver.com>
2024-10-28 09:53:15 +0900

    스쿼드메이커 포지션 위치값 수정

--- src/viewmodels/useFormationStyleViewModel.ts
+++ src/viewmodels/useFormationStyleViewModel.ts
@@ -2,7 +2,7 @@ export const useFormationStyleViewModel = () => {
     const formationPositionStyles: Record<string, Record<string, { top: string; left: string }[]>> = {
         // 3 Back
         '3-4-3': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '55%', left: '20%'},   // Left Center-back
                 {top: '55%', left: '50%'},   // Center-back
@@ -22,7 +22,7 @@ export const useFormationStyleViewModel = () => {
         },
 
         '3-4-3(2)': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '55%', left: '20%'},   // Left Center-back
                 {top: '55%', left: '50%'},   // Center-back
@@ -42,7 +42,7 @@ export const useFormationStyleViewModel = () => {
         },
 
         '3-4-1-2': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '55%', left: '20%'},   // Left Center-back
                 {top: '55%', left: '50%'},   // Center-back
@@ -62,27 +62,27 @@ export const useFormationStyleViewModel = () => {
         },
 
         '3-2-3-2': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
-                {top: '55%', left: '20%'},   // Left Center-back
-                {top: '55%', left: '50%'},   // Center-back
-                {top: '55%', left: '80%'},   // Right Center-back
+                {top: '60%', left: '20%'},   // Left Center-back
+                {top: '60%', left: '50%'},   // Center-back
+                {top: '60%', left: '80%'},   // Right Center-back
             ],
             midfielders: [
-                {top: '40%', left: '40%'},   // Defensive-midfield
-                {top: '40%', left: '60%'},   // Defensive-midfield
-                {top: '40%', left: '20%'},   // Left-midfield
+                {top: '40%', left: '35%'},   // Defensive-midfield
+                {top: '40%', left: '65%'},   // Defensive-midfield
+                {top: '25%', left: '20%'},   // Left-midfield
                 {top: '30%', left: '80%'},   // Right-midfield
                 {top: '25%', left: '50%'},   // Center-attacking-midfield
             ],
             forwards: [
-                {top: '10%', left: '40%'},   // Left Striker
-                {top: '10%', left: '60%'},   // Right Striker
+                {top: '5%', left: '40%'},   // Left Striker
+                {top: '5%', left: '60%'},   // Right Striker
             ]
         },
 
         '3-1-4-2': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '55%', left: '20%'},   // Left Center-back
                 {top: '55%', left: '50%'},   // Center-back
@@ -103,7 +103,7 @@ export const useFormationStyleViewModel = () => {
 
         // 4 Back
         '4-2-3-1': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '20%'},   // Left-back
                 {top: '55%', left: '35%'},   // Left Center-back
@@ -123,7 +123,7 @@ export const useFormationStyleViewModel = () => {
         },
 
         '4-4-2': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '20%'},   // Left-back
                 {top: '55%', left: '40%'},   // Left Center-back
@@ -143,17 +143,17 @@ export const useFormationStyleViewModel = () => {
         },
 
         '4-3-3': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '20%'},   // Left-back
-                {top: '55%', left: '35%'},   // Left Center-back
-                {top: '55%', left: '65%'},   // Right Center-back
+                {top: '60%', left: '40%'},   // Left Center-back
+                {top: '60%', left: '60%'},   // Right Center-back
                 {top: '60%', left: '80%'},   // Right-back
             ],
             midfielders: [
-                {top: '35%', left: '40%'},   // Left Center-midfield
-                {top: '35%', left: '50%'},   // Center-midfield
-                {top: '35%', left: '60%'},   // Right Center-midfield
+                {top: '35%', left: '35%'},   // Left Center-midfield
+                {top: '30%', left: '50%'},   // Center-midfield
+                {top: '35%', left: '65%'},   // Right Center-midfield
             ],
             forwards: [
                 {top: '10%', left: '20%'},   // Left-wing
@@ -163,31 +163,31 @@ export const useFormationStyleViewModel = () => {
         },
 
         '4-4-1-1': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '20%'},   // Left-back
-                {top: '55%', left: '35%'},   // Left Center-back
-                {top: '55%', left: '65%'},   // Right Center-back
+                {top: '60%', left: '40%'},   // Left Center-back
+                {top: '60%', left: '60%'},   // Right Center-back
                 {top: '60%', left: '80%'},   // Right-back
             ],
             midfielders: [
-                {top: '30%', left: '20%'},   // Left-midfield
-                {top: '30%', left: '40%'},   // Left Center-midfield
-                {top: '30%', left: '60%'},   // Right Center-midfield
-                {top: '30%', left: '80%'},   // Right-midfield
+                {top: '20%', left: '20%'},   // Left-midfield
+                {top: '35%', left: '35%'},   // Left Center-midfield
+                {top: '35%', left: '65%'},   // Right Center-midfield
+                {top: '20%', left: '80%'},   // Right-midfield
             ],
             forwards: [
-                {top: '20%', left: '50%'},   // Attacking-midfield
-                {top: '10%', left: '50%'},   // Striker
+                {top: '25%', left: '50%'},   // Attacking-midfield
+                {top: '5%', left: '50%'},   // Striker
             ]
         },
 
         '4-1-4-1': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '20%'},   // Left-back
-                {top: '55%', left: '35%'},   // Left Center-back
-                {top: '55%', left: '65%'},   // Right Center-back
+                {top: '60%', left: '40%'},   // Left Center-back
+                {top: '60%', left: '60%'},   // Right Center-back
                 {top: '60%', left: '80%'},   // Right-back
             ],
             midfielders: [
@@ -204,7 +204,7 @@ export const useFormationStyleViewModel = () => {
 
         // 5 Back
         '5-3-2': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '10%'},   // Left-wing-back
                 {top: '55%', left: '25%'},   // Left Center-back
@@ -224,7 +224,7 @@ export const useFormationStyleViewModel = () => {
         },
 
         '5-4-1': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '10%'},   // Left-wing-back
                 {top: '55%', left: '25%'},   // Left Center-back
@@ -244,7 +244,7 @@ export const useFormationStyleViewModel = () => {
         },
 
         '5-2-1-2': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '10%'},   // Left-wing-back
                 {top: '55%', left: '25%'},   // Left Center-back
@@ -264,7 +264,7 @@ export const useFormationStyleViewModel = () => {
         },
 
         '5-2-2-1': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '10%'},   // Left-wing-back
                 {top: '55%', left: '25%'},   // Left Center-back
@@ -284,7 +284,7 @@ export const useFormationStyleViewModel = () => {
         },
 
         '5-3-2(2)': {
-            goalkeeper: [{top: '74%', left: '50%'}],
+            goalkeeper: [{top: '76%', left: '50%'}],
             defenders: [
                 {top: '60%', left: '10%'},   // Left-wing-back
                 {top: '55%', left: '30%'},   // Left Center-back
