23e9a074edec65b6171991b18eba91f23c38c6c4
Harrison <wognsl305@naver.com>
2024-11-01 15:52:21 +0900

    센트리 최종 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,20 @@ import {BrowserRouter} from 'react-router-dom';
 import store, {persistor} from './store/store';
 import LoadingScreenHandler from './components/templates/LoadingScreenHandler';
 import ThemeProviderWrapper from './components/templates/ThemeProviderWrapper';
+import * as Sentry from "@sentry/react";
 import './App.css';
+
+Sentry.init({
+    dsn: "https://8aeb353d3ef73cf7a4e6715430f3f81c@o4508220889890816.ingest.us.sentry.io/4508220893560832",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: [/^https:\/\/mancityhub\.netlify\.app/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
 
 function App() {
     const basename = process.env.REACT_APP_BASENAME || '/';
