6043ae60be8cf8cd0d2ea7c915132499d11fadd2
Harrison <wognsl305@naver.com>
2024-11-01 14:50:31 +0900

    테스트

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,15 @@
+// src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
-import {BrowserTracing} from "@sentry/tracing";
 
 if (process.env.NODE_ENV === "production") {
     Sentry.init({
         dsn: "https://8aeb353d3ef73cf7a4e6715430f3f81c@o4508220889890816.ingest.us.sentry.io/4508220893560832",
-        integrations: [
-            new BrowserTracing({
-                routingInstrumentation: Sentry.reactRouterV6Instrumentation(),
-                tracePropagationTargets: ["localhost", /^https:\/\/mancityhub\.netlify\.app/],
-            }),
-            new Sentry.Replay(),
-        ],
+        integrations: [new Sentry.BrowserTracing()],
         tracesSampleRate: 1.0,
-        replaysSessionSampleRate: 0.1,
-        replaysOnErrorSampleRate: 1.0,
         release: "mancityhub@1.0.0",
         environment: process.env.NODE_ENV,
     });
