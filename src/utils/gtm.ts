62d8d2661e4bce5f87200d018bd4abac69b8d2df
Harrison <wognsl305@naver.com>
2024-11-01 12:10:42 +0900

    테스트

--- src/utils/gtm.ts
+++ src/utils/gtm.ts
@@ -6,29 +6,24 @@ declare global {
 }
 
 export const initializeGTM = (): void => {
-    if (process.env.NODE_ENV === 'production') {
-        // Initialize dataLayer if it doesn't exist
-        window.dataLayer = window.dataLayer || [];
+    window.dataLayer = window.dataLayer || [];
 
-        // Define the gtag function for Google Analytics as an arrow function
-        window.gtag = (...args: any[]) => {
-            window.dataLayer.push(args);
-        };
+    window.gtag = (...args: any[]) => {
+        window.dataLayer.push(args);
+    };
 
-        // Push initial page view event
-        window.dataLayer.push({
-            event: 'pageview',
-            page: window.location.pathname + window.location.search,
-        });
+    window.dataLayer.push({
+        event: 'pageview',
+        page: window.location.pathname + window.location.search,
+    });
 
-        // Add the Google Analytics (gtag.js) script dynamically
-        const gtagScript = document.createElement('script');
-        gtagScript.async = true;
-        gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=G-35LPSE99BQ`;
-        document.head.appendChild(gtagScript);
+    const gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=G-35LPSE99BQ`;
+    document.head.appendChild(gtagScript);
 
-        // Add inline script to configure gtag
-        window.gtag('js', new Date());
-        window.gtag('config', 'G-35LPSE99BQ');
-    }
+    // Add inline script to configure gtag
+    window.gtag('js', new Date());
+    window.gtag('config', 'G-35LPSE99BQ');
+
 };
