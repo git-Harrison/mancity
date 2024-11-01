eb3d8056a9217884e40addc41252fa0d3d82791f
Harrison <wognsl305@naver.com>
2024-11-01 14:11:33 +0900

    센트리 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
-import {BrowserRouter} from 'react-router-dom';
 import store, {persistor} from './store/store';
 import LoadingScreenHandler from './components/templates/LoadingScreenHandler';
 import ThemeProviderWrapper from './components/templates/ThemeProviderWrapper';
 import './App.css';
 
 function App() {
-    const basename = process.env.REACT_APP_BASENAME || '/';
-
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <BrowserRouter basename={basename}>
-                    <LoadingScreenHandler>
-                        <ThemeProviderWrapper/>
-                    </LoadingScreenHandler>
-                </BrowserRouter>
+                <LoadingScreenHandler>
+                    <ThemeProviderWrapper/>
+                </LoadingScreenHandler>
             </PersistGate>
         </Provider>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
+import {BrowserTracing} from "@sentry/tracing";
+import {BrowserRouter} from 'react-router-dom';
 
-
-Sentry.init({
-    dsn: "https://8aeb353d3ef73cf7a4e6715430f3f81c@o4508220889890816.ingest.us.sentry.io/4508220893560832",
-    integrations: [
-        Sentry.browserTracingIntegration(),
-        Sentry.replayIntegration(),
-    ],
-    tracesSampleRate: 1.0,
-    tracePropagationTargets: ["localhost", /^https:\/\/mancityhub\.netlify\.app/],
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-});
-
+if (process.env.NODE_ENV === "production") {
+    Sentry.init({
+        dsn: "https://8aeb353d3ef73cf7a4e6715430f3f81c@o4508220889890816.ingest.us.sentry.io/4508220893560832",
+        integrations: [
+            new BrowserTracing({
+                routingInstrumentation: Sentry.reactRouterV6Instrumentation(BrowserRouter),
+                tracePropagationTargets: ["localhost", /^https:\/\/mancityhub\.netlify\.app/],
+            }),
+            new Sentry.Replay(),
+        ],
+        tracesSampleRate: 1.0,
+        replaysSessionSampleRate: 0.1,
+        replaysOnErrorSampleRate: 1.0,
+        release: "mancityhub@1.0.0",
+        environment: process.env.NODE_ENV,
+    });
+}
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -24,7 +30,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
