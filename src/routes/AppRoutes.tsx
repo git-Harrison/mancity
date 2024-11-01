7f4aa6b2964433099b71c1687ac4ebd2f4de50bb
Harrison <wognsl305@naver.com>
2024-11-01 12:20:25 +0900

    구글 애널리틱스 테스트

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,21 +1,13 @@
-import React, {useEffect} from "react";
-import {Routes, Route, useLocation} from "react-router-dom";
+import React from "react";
+import {Routes, Route} from "react-router-dom";
 import MainPage from "../views/MainPage";
 import PlayerInfoPage from "../views/PlayerInfoPage";
 import ScoresPage from "../views/ScoresPage";
 import TransferMarket from "../views/TransferMarket";
 import PlayerEnhancement from "../views/PlayerEnhancement";
 import SquadSetup from "../views/SquadSetup";
-import {initializeGTM} from "../utils/gtm";
 
 const AppRoutes: React.FC = () => {
-    const location = useLocation();
-
-    useEffect(() => {
-        initializeGTM();
-        console.log(location.pathname);
-    }, [location]);
-
     return (
         <Routes>
             <Route path="/" element={<MainPage/>}/>
