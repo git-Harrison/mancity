c4de6918b7e5f951dd709f5a091a838749c6df6a
Harrison <wognsl305@naver.com>
2024-11-01 13:45:53 +0900

    테스트

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
 
-if (process.env.NODE_ENV === "production") {
-    Sentry.init({
-        dsn: "https://8aeb353d3ef73cf7a4e6715430f3f81c@o4508220889890816.ingest.us.sentry.io/4508220893560832",
-        integrations: [
-            Sentry.browserTracingIntegration(),
-            Sentry.replayIntegration(),
-        ],
-        tracesSampleRate: 1.0,
-        tracePropagationTargets: ["localhost", /^https:\/\/mancityhub\.netlify\.app/],
-        replaysSessionSampleRate: 0.1,
-        replaysOnErrorSampleRate: 1.0,
-    });
-}
+
+Sentry.init({
+    dsn: "https://8aeb353d3ef73cf7a4e6715430f3f81c@o4508220889890816.ingest.us.sentry.io/4508220893560832",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["localhost", /^https:\/\/mancityhub\.netlify\.app/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
