f0fca636f233f8c7b50311c19273b7e8663098c1
Harrison <wognsl305@naver.com>
2024-11-01 15:10:12 +0900

    테스트

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.MANCITY_LOCATION === "production") {
     Sentry.init({
         dsn: "https://8aeb353d3ef73cf7a4e6715430f3f81c@o4508220889890816.ingest.us.sentry.io/4508220893560832",
         integrations: [
